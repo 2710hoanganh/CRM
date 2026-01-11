2fc5db43d2781e08c5f9ec410dd728d3ab2ca72d
HoangGit <memayvtc001@hotmail.com>
2026-01-11 14:25:49 +0700

    loan

--- Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+124cd1ea086c127cb8f09db626f8a7b348339fb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83fa4f8542f307e73b3f1f9adb73f2032f1a76dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

82ecb78f193d76f159dca75733a6e14c776df8e4
HoangGit <memayvtc001@hotmail.com>
2026-01-17 19:01:07 +0700

    Add DTO test function

--- Presentation/Controllers/UserReferenceController.cs
+++ Presentation/Controllers/UserReferenceController.cs
@@ -6,6 +6,8 @@ using Domain.Constants.AppEnum;
 using Domain.Models.DTO.UserReference;
 using System.Security.Claims;
 using Application.Features.UserReference.Command;
+using Application.Features.UserReference.Query;
+using Presentation.DTOs;
 
 namespace Presentation.Controllers
 {
@@ -36,5 +38,22 @@ namespace Presentation.Controllers
                 Message = result.Result == ResponseResult.SUCCESS ? "User references created successfully" : "User references created failed"
             });
         }
+
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAllUserReferences([FromQuery] BasePaginationQueryDto query)
+        {
+            var result = await _mediator.Send(new GetUserReferenceQuery
+            {
+                Id = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? "0"),
+                PageNumber = query.PageNumber,
+                PageSize = query.PageSize
+            });
+            return Ok(new Response<Paged<List<GetUserReferenceResponse>>>(ResponseResult.SUCCESS)
+            {
+                Data = result,
+                Message = result.Message
+            });
+        }
+
     }
 }
